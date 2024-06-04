28c975a1bca666c990f4e448dba5beb20c7ad9f6
Ulephraim <iphraimunlimited@gmail.com>
2024-06-04 18:07:32 +0100

    modified backend

--- backend/server.js
+++ backend/server.js
@@ -12,13 +12,30 @@ import cloudinary from 'cloudinary';
 
 dotenv.config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 
+// const corsOptions = {
+//   origin: ['https://linker-social-media.vercel.app'],
+//   methods: ['POST', 'GET'],
+//   credentials: true,
+// };
+
 const corsOptions = {
-  origin: ['https://linker-social-media.vercel.app'],
-  methods: ['POST', 'GET'],
+  origin: (origin, callback) => {
+    // Check if the origin matches the pattern *.vercel.app
+    if (
+      origin &&
+      (origin.match(/^https?:\/\/(.*\.)?vercel\.app$/) ||
+        origin === 'http://localhost:3000')
+    ) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true,
 };
 
